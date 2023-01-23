5855f9c5ada4f1b592ba8ec1add61fb620d3209f
Kalamadi Rajakumar Prakash <118983558+RajakumarKalamadi@users.noreply.github.com>
2023-01-24 00:41:13 +0530

    Multiply program

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+package Intorduction_Assignment;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 2;
+		int y = 4;
+		System.out.println(x*y);
+	}
+
+}
