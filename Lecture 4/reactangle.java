a43592314311ae21f480b684827ebff2e78eaeaa
Kalamadi Rajakumar Prakash <118983558+RajakumarKalamadi@users.noreply.github.com>
2023-01-24 00:51:09 +0530

    3rd question

---
+++ Lecture 4/reactangle.java
@@ -0,0 +1,12 @@
+package Intorduction_Assignment;
+
+public class reactangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int length = 7;
+		int width = 4;
+		System.out.println(length * width);
+	}
+
+}
