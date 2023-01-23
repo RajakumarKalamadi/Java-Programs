5ae186eb7835a327106f634842f1092a7ceddaca
Kalamadi Rajakumar Prakash <118983558+RajakumarKalamadi@users.noreply.github.com>
2023-01-24 00:59:02 +0530

    swapping

---
+++ Lecture 4/swap_two_number.java
@@ -0,0 +1,12 @@
+package Intorduction_Assignment;
+
+public class swap_two_number {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int  num3 = num1;
+		num1 = num2;
+		num2 = num3;
+		System.out.println(num1 + " "+ num2);
+	}
+}
