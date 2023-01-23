60dad0851a8fefcfc103d8ca2e22dce074f8bbdd
Kalamadi Rajakumar Prakash <118983558+RajakumarKalamadi@users.noreply.github.com>
2023-01-24 00:48:55 +0530

    2nd question

---
+++ Lecture 4/ascii_value.java
@@ -0,0 +1,9 @@
+package Intorduction_Assignment;
+
+public class ascii_value {
+	public static void main(String[] args) {
+		char ascii = 'U';
+		int value = ascii;
+		System.out.println(value);
+	}
+}
