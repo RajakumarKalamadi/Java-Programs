5886b7395fde23a7389e3775d7ad6730a7846925
Kalamadi Rajakumar Prakash <118983558+RajakumarKalamadi@users.noreply.github.com>
2023-01-24 00:56:09 +0530

    size of data types

---
+++ Lecture 4/cube.java
@@ -0,0 +1,12 @@
+package Intorduction_Assignment;
+
+public class cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int _byte = 4;
+		int bit = 8;
+		System.out.println((_byte * bit));
+	}
+
+}
