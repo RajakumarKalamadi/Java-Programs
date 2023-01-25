194676df7dfc44cbd27e55cd992960652fbe53f5
Kalamadi Rajakumar Prakash <118983558+RajakumarKalamadi@users.noreply.github.com>
2023-01-25 23:18:02 +0530

    OOPS Assignment

---
+++ Lecture 5/studentUse.java
@@ -0,0 +1,15 @@
+package OOPS;
+class student{
+	String name = "john";
+	int roll_num = 2;
+}
+public class studentUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		student s1 = new student();
+		System.out.println(s1.name);
+		System.out.println(s1.roll_num);
+	}
+
+}
