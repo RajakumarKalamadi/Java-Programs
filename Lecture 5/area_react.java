fcf605e4cb798e70f9a82f07cc5e257ba4361217
Kalamadi Rajakumar Prakash <118983558+RajakumarKalamadi@users.noreply.github.com>
2023-01-25 23:43:35 +0530

    Add files via upload

---
+++ Lecture 5/area_react.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+public class area_react {
+	
+	public area_react(int length, int width) {
+		
+		int area = length * width;
+		System.out.println(area);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		area_react ar = new area_react(4,5);
+		
+	}
+
+}
